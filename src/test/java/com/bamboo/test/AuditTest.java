91327c1cdf0fdf611d7723f9414849d11d136ba3
Alexander Bonilla <aldaboad@gmail.com>
2018-11-19 15:05:01 -0500

    Modelo Audit creado

---
+++ src/test/java/com/bamboo/test/AuditTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bamboo.test;
+
+import com.bamboo.data.OperatorData;
+import com.bamboo.model.entity.dependent.Audit;
+import org.junit.Test;
+
+import com.bamboo.model.method.dependent.AuditImpl;
+import com.bamboo.model.entity.dependent.Beneficiary;
+import com.bamboo.model.entity.dependent.Operator;
+
+/**
+ *
+ * @author alexander
+ */
+public class AuditTest {
+
+    private final AuditImpl audit = new AuditImpl(Beneficiary.class);
+    private final OperatorData operatorData = new OperatorData();
+    private final Operator operator;
+
+    public AuditTest() {
+        operatorData.save();
+        operator = operatorData.findById();
+
+    }
+
+    @Test
+    public void run() {
+        printTitle("Audit");
+
+        audit.save(new Audit(operator, "description"));
+        System.out.println("");
+        audit.Update(new Audit(operator, "description"));
+        System.out.println("");
+        audit.delete(new Audit(operator, "description"));
+        System.out.println("");
+        operatorData.delete();
+
+        System.out.println("\n\n");
+    }
+
+    private void printTitle(String title) {
+        System.out.print("\n\n\033[1m-----------------------------------< \033[0m");
+        System.out.print("\033[36m" + title + " \033[0m");
+        System.out.println("\033[1m>-----------------------------------\033[0m\n");
+
+    }
+}
