62b16857a3d4d7dbc9fb194820aa8ef872e23510
Alexander Bonilla <aldaboad@gmail.com>
2018-11-25 23:47:19 -0500

    ruta pageNotFound

---
+++ web/src/components/error.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="not-found-screen">
+    <div id="clouds">
+      <div class="cloud x1"></div>
+      <div class="cloud x1_5"></div>
+      <div class="cloud x2"></div>
+      <div class="cloud x3"></div>
+      <div class="cloud x4"></div>
+      <div class="cloud x5"></div>
+    </div>
+    <div class='c'>
+      <div class='_404'>404</div>
+      <hr>
+      <div class='_1'>LA PÁGINA</div>
+      <div class='_2'>NO FUE ENCONTRADA</div>
+    </div>
+  </div>
+</template>
+
+<style lang="css">
+  @import '../assets/css/error.css';
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login'
+import Admin from '@/components/admin'
+import PageNotFound from'@/components/error'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin
+    }
+    ,
+    {
+      path: '*',
+      name: '*',
+      component: PageNotFound
     }
   ]
 })
