4a9d9ce5a51acc68a0f655b67d4f071420566db5
Alexander Bonilla <aldaboad@gmail.com>
2019-04-18 18:53:49 -0500

    FrontEnd LoginView The login view created only the .vue and .sass Also Home.vue and About.vue deleted

---
+++ front/src/views/login/LoginView.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app class="login primary">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4 lg4>
+            <v-card class="elevation-12">
+              <form @submit.prevent="login()">
+                <v-card-text>
+                  <div class="layout column align-center">
+                    <img src="@/assets/logo.svg" alt="Bamboo" width="120" height="120">
+                    <h1 class="flex my-4 primary--text">Bamboo</h1>
+                  </div>
+                  <v-form>
+                    <v-text-field
+                      color="primary"
+                      append-icon="person"
+                      name="login"
+                      label="Usuario"
+                      type="text"
+                      v-model="username"
+                    ></v-text-field>
+                    <v-text-field
+                      color="primary"
+                      append-icon="lock"
+                      name="password"
+                      label="Contraseña"
+                      id="password"
+                      type="password"
+                      v-model="password"
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn block color="primary white--text" type="submit">entrar</v-btn>
+                </v-card-actions>
+              </form>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<!--script lang="ts">
+import LoginView from '@/views/login/LoginView.ts';
+export default LoginView;
+</script-->
+
+<style lang="sass">
+    @import "@/views/login/LoginView.sass";
+</style>
