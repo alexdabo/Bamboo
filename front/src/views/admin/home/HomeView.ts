c8fa2fef04577c5baea2f8cd3a47e3c3c109a5d4
Alexander Bonilla <aldaboad@gmail.com>
2019-04-20 11:22:05 -0500

    home images

---
+++ front/src/views/admin/home/HomeView.ts
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+interface Item {
+    imageName: string;
+    title: string;
+    routerName: string;
+}
+
+@Component({ name: 'view-home' })
+
+export default class HomeView extends Vue {
+    public viewItems: Item[] = [
+      { imageName: 'dashboard', title: 'Dashboard', routerName: 'dashboard' },
+      { imageName: 'invoice', title: 'Cuentas', routerName: 'accounts' },
+      { imageName: 'user', title: 'Usuarios', routerName: 'users' },
+      { imageName: 'beneficiaries', title: 'Beneficiarios', routerName: 'beneficiaries' },
+      { imageName: 'measurer', title: 'Medidores', routerName: 'measurers' },
+      { imageName: 'services', title: 'Servicios', routerName: 'services' },
+      { imageName: 'reports', title: 'Reportes', routerName: 'reports' }
+    ];
+
+    public changeView (routerName: string): void {
+      if (routerName === 'measurers') {
+        this.$router.push({ name: routerName, params: { beneficiaryId: '0' } })
+      }
+      this.$router.push({ name: routerName })
+    }
+
+    public getImgUrl (name: string): any {
+      if (name === undefined) return null
+      var images = require.context(
+        '@/assets/image/home/',
+        false,
+        /\.png$/
+      )
+      return images('./' + name + '.png')
+    }
+}
---
+++ front/src/views/admin/home/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid padding>
+    <v-layout row wrap>
+      <v-flex v-for="(item,index) in viewItems" :key="index" xs12 sm3 md3 class="pa-2">
+        <v-card @click="changeView(item.routerName)" style="cursor:pointer;">
+          <v-card-title>
+            <v-spacer/>
+            <img :src="getImgUrl(item.imageName)" alt>
+            <v-spacer/>
+          </v-card-title>
+          <v-divider light></v-divider>
+          <v-card-actions class="pa-3 center">
+            <v-spacer></v-spacer>
+            <h3>{{item.title}}</h3>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script lang="ts">
+import HomeView from '@/views/admin/home/HomeView.ts'
+export default HomeView
+</script>
