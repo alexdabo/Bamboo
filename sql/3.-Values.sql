9eb81f1be9c7d9c2a6d132bc5bc726e6ca18ea9d
Alexander Bonilla <aldaboad@gmail.com>
2019-04-29 10:34:12 -0500

    status 4 sin servicio

--- sql/3.-Values.sql
+++ sql/3.-Values.sql
@@ -6,7 +6,8 @@ INSERT INTO role(name) VALUES
 INSERT INTO status(name) VALUES
   ('Activo'),
   ('Suspendido'),
-  ('Dañado');
+  ('Dañado'),
+  ('Sin servicio');
 
 INSERT INTO public.operator(roleid,username, password, email, dni, firstname, lastname, telephone, address) VALUES
        (1,'admin', '21232f297a57a5a743894a0e4a801fc3','admin@bamboo.com', '0000000000', 'admin', 'admin', '0000000000','');
--- sql/4.-Triggers.sql
+++ sql/4.-Triggers.sql
@@ -1,70 +1,88 @@
 /*******************************************************************
 *                    CALCULAR VALORES DE MEDIDAS                   *
 ********************************************************************/
-CREATE OR REPLACE FUNCTION public.fun_calc_uptake() RETURNS TRIGGER AS $BODY$
+CREATE OR REPLACE FUNCTION public.fun_calc_uptake() RETURNS TRIGGER AS
+$BODY$
 declare
-  meter_status  INTEGER ;
-  assigned_id INTEGER;
-  assigned_debt DOUBLE PRECISION;
+    meter_status  INTEGER ;
+    assigned_id   INTEGER;
+    assigned_debt DOUBLE PRECISION;
 BEGIN
 
-  meter_status = (select statusid from measurer where id = new.measurerid);
-  assigned_id= (select id from assigned where measurerid = new.measurerid and status = 'enable');
-  assigned_debt= (select debt from assigned where id = assigned_id);
+    meter_status = (select statusid from measurer where id = new.measurerid);
+    assigned_id = (select id from assigned where measurerid = new.measurerid and status = 'enable');
+    assigned_debt = (select debt from assigned where id = assigned_id);
 
-  new.lastvaluetaken = (select currentvaluetaken from uptake where measurerid=new.measurerid order by id  desc limit 1);
-  if new.lastvaluetaken is null then
-    new.lastvaluetaken = 0;
-  end if;
+    new.lastvaluetaken =
+            (select currentvaluetaken from uptake where measurerid = new.measurerid order by id desc limit 1);
 
-	new.basevolume=(select s.basevolume from measurer m inner join sap s on s.id=m.sapid where m.id=new.measurerid);
+    if new.lastvaluetaken is null then
+        new.lastvaluetaken = 0;
+    end if;
 
-	new.baseprice= (select s.baseprice  from measurer m inner join sap s on s.id=m.sapid where m.id=new.measurerid);
+    new.basevolume = (select s.basevolume
+                      from measurer m
+                               inner join sap s on s.id = m.sapid
+                      where m.id = new.measurerid);
 
-	new.extraprice=(select s.extraprice from measurer m inner join sap s on s.id=m.sapid where m.id=new.measurerid);
+    new.baseprice = (select s.baseprice
+                     from measurer m
+                              inner join sap s on s.id = m.sapid
+                     where m.id = new.measurerid);
 
+    new.extraprice = (select s.extraprice
+                      from measurer m
+                               inner join sap s on s.id = m.sapid
+                      where m.id = new.measurerid);
 
 
     --When the state is active
     if meter_status = 1 then
-      if new.currentvaluetaken >= new.lastvaluetaken then
-        new.volumeconsumed = (new.currentvaluetaken - new.lastvaluetaken);
-        if new.volumeconsumed > new.basevolume then
-          new.volumeexceeded = (new.volumeconsumed - new.basevolume);
-          new.totalprice = (new.baseprice + (new.volumeexceeded * new.extraprice));
-          update assigned set debt = assigned_debt+new.totalprice where id=assigned_id;
+        if new.currentvaluetaken >= new.lastvaluetaken then
+            new.volumeconsumed = (new.currentvaluetaken - new.lastvaluetaken);
+            if new.volumeconsumed > new.basevolume then
+                new.volumeexceeded = (new.volumeconsumed - new.basevolume);
+                new.totalprice = (new.baseprice + (new.volumeexceeded * new.extraprice));
+                update assigned set debt = assigned_debt + new.totalprice where id = assigned_id;
+            else
+                new.totalprice = new.baseprice;
+                new.volumeexceeded = 0;
+                update assigned set debt = (assigned_debt + new.totalprice) where id = assigned_id;
+            end if;
         else
-          new.totalprice = new.baseprice;
-          new.volumeexceeded = 0;
-          update assigned set debt = (assigned_debt+new.totalprice) where id=assigned_id;
+            RAISE EXCEPTION 'Medida actual menor a la anterior';
         end if;
-      else
-          RAISE EXCEPTION 'Medida actual menor a la anterior';
-      end if;
     end if;
 
     --When the state is suspended
     if meter_status = 2 then
-      RAISE EXCEPTION 'Servicio cortado en el medidor N° %',new.measurerid;
+        RAISE EXCEPTION 'Servicio cortado en el medidor N° %',new.measurerid;
     end if;
 
     --When the state is damaged
-    if meter_status =3 then
-      new.currentvaluetaken = new.lastvaluetaken;
-      new.volumeexceeded = 0;
-      new.volumeconsumed = new.basevolume;
-      new.totalprice = new.baseprice;
-      update assigned set debt = (assigned_debt+new.totalprice) where id=assigned_id;
+    if meter_status = 3 then
+        new.currentvaluetaken = new.lastvaluetaken;
+        new.volumeexceeded = 0;
+        new.volumeconsumed = new.basevolume;
+        new.totalprice = new.baseprice;
+        update assigned set debt = (assigned_debt + new.totalprice) where id = assigned_id;
     end if;
 
-    if meter_status > 3 then
-      RAISE EXCEPTION 'No existe reglas para sap con id=%', meter_status;
+    if meter_status = 4 then
+        RAISE EXCEPTION 'Medidor sin servicio';
     end if;
 
- 	return new;
+    if meter_status > 4 then
+        RAISE EXCEPTION 'No existe reglas para sap con id=%', meter_status;
+    end if;
+
+    return new;
 
 END
 $BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER calc_uptake before insert  on uptake
-FOR EACH ROW EXECUTE PROCEDURE fun_calc_uptake();
+CREATE TRIGGER calc_uptake
+    before insert
+    on uptake
+    FOR EACH ROW
+EXECUTE PROCEDURE fun_calc_uptake();
